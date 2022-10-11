b21fcc1a69a6a8e0100bd8db190914a5c565b6d8
up1 <somkiat.p@gmail.com>
2022-10-11 17:23:03 +0700

    Call real api

--- rest_api_echo/users/user_test.go
+++ rest_api_echo/users/user_test.go
@@ -15,12 +15,32 @@ import (
 func TestSuccessWithGet(t *testing.T) {
 	// Start server
 	e := echo.New()
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/users", nil)
+	c := e.NewContext(req, rec)
+
+	service := users.NewUserService()
+	users.GetUserHandler(service)(c)
+
+	// Assert
+	assert.Equal(t, rec.Code, 200)
+	assert.Contains(t, rec.Body.String(), "Call get user")
+}
+
+func TestSuccessWithGetWithRealServer(t *testing.T) {
+	// Setup router
+	e := echo.New()
 	service := users.NewUserService()
 	e.GET("/users", users.GetUserHandler(service))
 
+	// Call API
 	rec := httptest.NewRecorder()
-	httptest.NewRequest(http.MethodGet, "/users", nil)
+	req := httptest.NewRequest(http.MethodGet, "/users", nil)
+
+	// Start server
+	e.ServeHTTP(rec, req)
 
 	// Assert
-	assert.Equal(t, 201, rec.Code)
+	assert.Equal(t, rec.Code, 200)
+	assert.Contains(t, rec.Body.String(), "Call get user")
 }
